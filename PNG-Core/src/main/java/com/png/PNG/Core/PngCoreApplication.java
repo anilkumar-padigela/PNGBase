d0af04d6b6a42d89e798e13eb862843ec498bb19
Anil Kumar Padigela <anil0407@hotmail.com>
2019-08-28 05:45:36 +0530

    First Commit : Committing the Project structure

---
+++ PNG-Core/src/main/java/com/png/PNG/Core/PngCoreApplication.java
@@ -0,0 +1,13 @@
+package com.png.PNG.Core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PngCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PngCoreApplication.class, args);
+	}
+
+}
---
+++ PNG-UI/src/main/java/com/png/PNG/UI/PngUiApplication.java
@@ -0,0 +1,13 @@
+package com.png.PNG.UI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PngUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PngUiApplication.class, args);
+	}
+
+}
